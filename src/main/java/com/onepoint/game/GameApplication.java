7976290259339543e22ac6c4a030764cc4b7ce6c
Len <len.ochiai@gmail.com>
2021-06-20 22:52:54 +0200

    added a 2nd player

--- src/main/java/com/onepoint/game/GameApplication.java
+++ src/main/java/com/onepoint/game/GameApplication.java
@@ -14,9 +14,10 @@ public class GameApplication {
 	public static void main(String[] args) throws InvalidSequenceException {
 		Player mike = new Player("Mike");
 		mike.setBall(new SequenceBall(args));
+		Player john = new Player("John");
 		GameService service = new GameService();
 		Game game = service.startNewGame();
-
+		service.addPlayer(john, game);
 		service.addPlayer(mike, game);
 		service.playGame(game);
 
